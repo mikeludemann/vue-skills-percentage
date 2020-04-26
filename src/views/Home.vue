df90256496aee295be27dbaa701716345fd3f6b6
Mike Ludemann <mike-ludemann@hotmail.de>
2020-04-26 17:37:30 +0200

    Setup complete - vue-skills-percentage

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<circlePercentage :percentage="57" color="red" :dark="false"/>
+		<skillsPercentage :percentage="57"/>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import circlePercentage from '@/components/circle-percentage.vue';
+import skillsPercentage from '@/components/skills-percentage.vue';
+
+export default {
+	name: 'Home',
+	components: {
+		HelloWorld,
+		circlePercentage,
+		skillsPercentage
+	}
+}
+</script>
